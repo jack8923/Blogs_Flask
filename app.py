056ad9d3178f156a9fa58a9b8b5fa921bc851d52
Rohit Chandwani <rohit.chandwani@UE-B-NOTE-043.local>
2020-06-07 15:13:14 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template, request, redirect
-from models import BlogPost, db
+# from models import BlogPost, db
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
+
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(20), nullable=False, default='N/A')
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog post ' + str(self.id)
 
 
 @app.route('/')
