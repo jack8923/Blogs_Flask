a3d0640ecc74693dee3caf6cde25de68d4b47c14
Rohit Chandwani <rohit.chandwani@UE-B-NOTE-043.local>
2020-06-10 10:41:51 +0530

    added manage.py

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
 from flask import Flask, render_template, request, redirect
-# from models import BlogPost, db
+from models import BlogPost
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/posts'
 db = SQLAlchemy(app)
-
-
-class BlogPost(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(20), nullable=False, default='N/A')
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    def __repr__(self):
-        return 'Blog post ' + str(self.id)
 
 
 @app.route('/')
@@ -24,7 +15,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/posts')
+@app.route('/posts/')
 def posts():
     posts = BlogPost.query.order_by(BlogPost.date_posted).all()
     return render_template('posts.html', posts=posts)
